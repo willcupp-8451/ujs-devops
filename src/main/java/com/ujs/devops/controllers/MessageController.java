f5d38256107a4ff944661adfd8ea29b39d38052a
caseyokane-8451 <casey.o'kane@8451.com>
2021-06-08 07:55:05 -0400

    Adding completed files from previous branch

--- src/main/java/com/ujs/devops/controllers/MessageController.java
+++ src/main/java/com/ujs/devops/controllers/MessageController.java
@@ -1,5 +1,6 @@
 package com.ujs.devops.controllers;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MessageController {
 
   @GetMapping("/message")
-  public String getVariableString() {
-    return "this is staging";
+  public String getMessage(@Value("${e451.my-message}") String myMessage) {
+    return myMessage;
   }
 }
